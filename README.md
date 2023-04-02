0d6e51d01de4d5bf1859888a9d01cb02d174a0ea
Buddylasta <thecallousedcoin@protonmail.com>
2023-04-01 21:51:34 -0500

    finished api tutorial code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # go-api
 Tutorial: Developing a RESTful API with Go and Gin. Will hopefully turn this into a bitcoin-related project
+
+# Endpoints
+/albums
+
+    GET – Get a list of all albums, returned as JSON.
+    POST – Add a new album from request data sent as JSON.
+
+/albums/:id
+
+    GET – Get an album by its ID, returning the album data as JSON.
+
+
+# Steps
+[Tutorial](https://go.dev/doc/tutorial/web-service-gin)
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	//Initialize a Gin router using Default.
+	router := gin.Default()
+
+	//Use the GET function to associate the GET HTTP method and /albums path with a handler function
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
+
+	//Use the Run function to attach the router to an http.Server and start the server.
+	router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+// gin.Context carries request details, validates and serializes JSON, and more
+func getAlbums(c *gin.Context) {
+	//serialize the struct into JSON and add it to the response.
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
